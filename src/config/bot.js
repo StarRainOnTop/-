fe112f676f9377428e16274001058afc0fd7f861
StarRainOnTop <132820808+StarRainOnTop@users.noreply.github.com>
2026-08-13 22:57:33 +0800

    bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,9 +23,9 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "Streaming", // required by Discord API, not shown in the client
         state: "正在跟星雨玩床戰",     // this is what people actually see
-        type: 1,               // Custom
+        type: 0,               // Custom
       },
     ],
   },
