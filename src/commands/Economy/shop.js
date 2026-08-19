52570645d831d81b469416b1fe6ed8fe36314d7f
StarRainOnTop <132820808+StarRainOnTop@users.noreply.github.com>
2026-08-20 00:23:28 +0800

    shop.js

--- src/commands/Economy/shop.js
+++ src/commands/Economy/shop.js
@@ -5,7 +5,7 @@ export default {
     slashOnly: true,
     data: new SlashCommandBuilder()
         .setName('shop')
-        .setDescription('Browse the economy shop.'),
+        .setDescription('瀏覽經濟商店。'),
 
     async execute(interaction, config, client) {
         return shopBrowse.execute(interaction, config, client);
