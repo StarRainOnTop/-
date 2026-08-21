2870698f5a803e04c1e07e068092385e643ce269
StarRainOnTop <132820808+StarRainOnTop@users.noreply.github.com>
2026-08-21 18:24:56 +0800

    shop.js

--- src/commands/Economy/shop.js
+++ src/commands/Economy/shop.js
@@ -1,28 +1,14 @@
 import { SlashCommandBuilder } from 'discord.js';
-import shopConfigSetrole from './modules/shop_config_setrole.js';
+import shopBrowse from './modules/shop_browse.js';
+import { withErrorHandling } from '../../utils/errorHandler.js';
 
 export default {
     slashOnly: true,
     data: new SlashCommandBuilder()
-        .setName('shop-config')
-        .setDescription('設定商店選項。（需要「管理伺服器」權限）')
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('setrole')
-                .setDescription('設定購買高級身分組商店道具時所授予的 Discord 身分組。')
-                .addRoleOption(option =>
-                    option
-                        .setName('role')
-                        .setDescription('購買高級身分組時要授予的身分組。')
-                        .setRequired(true),
-                ),
-        ),
+        .setName('shop')
+        .setDescription('瀏覽經濟商店。'),
 
-    async execute(interaction, config, client) {
-        const subcommand = interaction.options.getSubcommand();
-
-        if (subcommand === 'setrole') {
-            return shopConfigSetrole.execute(interaction, config, client);
-        }
-    },
+    execute: withErrorHandling(async (interaction, config, client) => {
+        return shopBrowse.execute(interaction, config, client);
+    }, { command: 'shop' })
 };
