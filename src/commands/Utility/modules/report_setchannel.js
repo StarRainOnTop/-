05c50cfb114ff142b1b946727ccbb001d4e67dda
StarRainOnTop <132820808+StarRainOnTop@users.noreply.github.com>
2026-08-23 02:58:06 +0800

    report_setchannel.js

--- src/commands/Utility/modules/report_setchannel.js
+++ src/commands/Utility/modules/report_setchannel.js
@@ -8,7 +8,7 @@ import { replyUserError, ErrorTypes } from '../../../utils/errorHandler.js';
 export default {
     async execute(interaction, config, client) {
         if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageGuild)) {
-            return await replyUserError(interaction, { type: ErrorTypes.PERMISSION, message: 'You need **Manage Server** permissions to set the report channel.' });
+            return await replyUserError(interaction, { type: ErrorTypes.PERMISSION, message: '你需要 **管理伺服器** 權限才能設定檢舉頻道。' });
         }
 
         const channel = interaction.options.getChannel('channel');
@@ -19,14 +19,14 @@ export default {
 
             return InteractionHelper.safeReply(interaction, {
                 embeds: [successEmbed(
-                    'Report Channel Set',
-                    `All new reports will now be sent to ${channel}.\nYou can also manage this from \`/logging dashboard\`.`,
+                    '已設定檢舉頻道',
+                    `所有新的檢舉現在都會傳送到 ${channel}。\n你也可以從 \`/logging dashboard\` 來管理此設定。`,
                 )],
                 ephemeral: true,
             });
         } catch (error) {
             logger.error('report_setchannel error:', error);
-            return await replyUserError(interaction, { type: ErrorTypes.UNKNOWN, message: 'Could not save the channel configuration.' });
+            return await replyUserError(interaction, { type: ErrorTypes.UNKNOWN, message: '無法儲存頻道設定。' });
         }
     },
 };
